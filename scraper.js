7a67a679a0804e9c63cd22b3f5d1910dedfb016a
HOss3inf <hossein.2006man@gmail.com>
2019-01-30 08:26:25 +0330

    sequences were labeled based on their family and subfamily and subsubfamily

--- scraper.js
+++ scraper.js
@@ -1,39 +1,21 @@
 const fs = require('fs');
-const lineByLine = require('line-by-line')
 let filelist = fs.readdirSync('./data/','utf-8','r');
-let writeStream = fs.createWriteStream('./sequences.txt')
-// console.log(filelist.length);
+let writeStream = fs.createWriteStream('./sequences.txt');
 
 for (value of filelist) {
-//    console.log('filelist:', filelist);
    let filename = extensionRemover(value);
-   // console.log('filename:', filename);
    [family,sub,subsub] = filename.split('_');
-   // console.log(family, sub, subsub);
-let sequence;
-    lr = new lineByLine('./data/'+ value,{encoding: 'utf-8',skipEmptyLines: true});
-    lr.on('line', (line) => {
-        
-       if(!line.includes('gi')) {
-            // console.log('line:', line,'\n');
-        sequence = line;};
-// console.log('line:', line,'\n');
-protein = family + ',' + sub + ',' + subsub + ',' + sequence + '\n';
-// console.log('protein:', protein);
+fs.readFileSync('./data/'+value).toString().split('\n').forEach(function (line) { 
+    if(!line.includes('gi') && line.length>1) {
+protein = family + ',' + sub + ',' + subsub + ',' + line + '\n';
 writeStream.write(protein);
-
-    })
-
 };
-// writeStream.close();
-
-
-
-
+ });
+};
 
 function extensionRemover(filename){
 return filename.split('.')[0];
 }
 
-// console.log(filenames);
+
 
